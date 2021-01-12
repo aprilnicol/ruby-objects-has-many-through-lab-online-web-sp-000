e54e6d4e823ecf1958ff7bda1d82840531aab6cb
April Adams <illusionlace@gmail.com>
2021-01-12 21:39:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
-end 
+end
   def patients
     appointments.map {|appointment| appointment.patient}
   end
