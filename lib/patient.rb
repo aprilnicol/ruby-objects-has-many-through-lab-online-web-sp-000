a96ff159cbf3bbed7953769ddf2d5d97bd97b21a
April Adams <illusionlace@gmail.com>
2021-01-12 22:17:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,19 +1,20 @@
 class Patient
-	attr_accessor :name
-	def initialize(name)
-		@name = name
-		@appointments = []
-	end
-	def appointments
-		@appointments
-	end
-	def add_appointment(appointment)
-		@appointments << appointment
-		appointment.patient = self
-	end
-	def doctors
-		@appointments.collect do |a|
-			a.doctor
-		end
-	end
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
 end
