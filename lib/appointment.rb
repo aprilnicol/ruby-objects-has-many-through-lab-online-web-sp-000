9ec16e2fe6016c76c6472bf2bb328295c968964e
April Adams <illusionlace@gmail.com>
2021-01-12 21:18:11 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,13 @@
 class Appointment
-  attr_accessor :date, :patient, :doctor 
+  attr_accessor :date, :patient, :doctor
   @@all = []
   def initialize(patient, date, doctor)
-    @patient = patient 
-    @date = date 
-    @doctor = doctor 
-    @@all << self 
-  end 
-  def self.all 
-    @all 
-  end 
+    @patient = patient
+    @date = date
+    @doctor = doctor
+    @@all << self
+  end
+  def self.all
+    @all
+  end
 end 
